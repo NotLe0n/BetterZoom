f1abebe8407e9aaf2343587415ae5f13c17d9ee4
NotLe0n <notle0n75@gmail.com>
2022-11-20 14:42:19 +0100

    added config tooltips

--- src/Config.cs
+++ src/Config.cs
@@ -10,24 +10,28 @@ internal class Config : ModConfig
 	public override ConfigScope Mode => ConfigScope.ClientSide;
 
 	[Label("Scale Background")]
+	[Tooltip("This toggles if the background should zoom too")]
 	public bool scaleBackground;
 
 	[Slider]
 	[Range(0.1f, 10.0f)]
 	[DefaultValue(1f)]
+	[Tooltip("This changes the size of your cursor")]
 	[Label("Cursor Scale")]
 	public float cursorScale;
 	
 	[Range(0.3f, 10.0f)]
 	[DefaultValue(0.3f)]
 	[ReloadRequired]
-	[Label("min zoom")]
+	[Tooltip("This changes the minimum amount of zoom you can set\nYou need to set this in the mod menu, since it requires a reload")]
+	[Label("Min Zoom")]
 	public float minZoom;
 	
+	[ReloadRequired]
 	[Range(0.3f, 10.0f)]
 	[DefaultValue(10f)]
-	[ReloadRequired]
-	[Label("max zoom")]
+	[Tooltip("This changes the maximal amount of zoom you can set\nYou need to set this in the mod menu, since it requires a reload")]
+	[Label("Max Zoom")]
 	public float maxZoom;
 
 	public override void OnChanged()
